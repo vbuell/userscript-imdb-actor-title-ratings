72939c0b4bd2f346a227a480de19b140f8290b1b
Volodymyr Buell <vbuell@gmail.com>
2016-05-07 09:28:55 -0400

    Import from usercripts_org

--- imdb.actors.ratings.user.js
+++ imdb.actors.ratings.user.js
@@ -17,11 +17,12 @@ var css = '<style type="text/css"> \
              .year_column{width:40px;text-align:inherit} \
              .header .sorted{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAICAYAAADN5B7xAAAAQUlEQVR4AWOQTNtkAMT/icQGDCAAZDQQobiBAQpgmi7gUXyBAQ0QcpoBunp8TmtAV4fhNHxOIeA0TKfgdBoupwAAVdttkcoQnXIAAAAASUVORK5CYII=) left no-repeat} \
              .header .sortedDesc{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAICAYAAADN5B7xAAAAPUlEQVR4AWPABiTTNjWAMLo4LsUGQPwfig2I0XABScMFQoob4IoRuIGwUzCxAX6nYOILBJ2C1WmEnYLpNADzi22RZiYJ+gAAAABJRU5ErkJggg==) left no-repeat} \
-          </style>'
+          </style>';
 $(css).appendTo('head');
 
 // add headers
 $('.filmo-category-section').each(function() {
+	'use strict';
 	if ($(this).find('div.filmo-row').length > 1) {
 		$(this).prepend('<div class="filmo-row header"><div class="year_column sorted"><a>Year</a></div><div class="votes_column mellow"><a>Votes</a></div><div class="rating_column"><a>Rating</a></div><br></div>');
 	}
@@ -31,17 +32,19 @@ $('.filmo-category-section').each(function() {
 
 //  add the rating & votes to the movie/TV show
 function addData(yearSpan, rating, votes, released) {
+	'use strict';
     $(yearSpan).attr('title', released);
 	$(yearSpan).after('<span class="votes_column mellow"><small>' + votes + '</small></span><span class="rating_column">' + rating + '</span>');
 }
 
 // iterate the movies/TV shows and lookup the rating & vote count via omdbApi.com
 $('.filmo-row:not(.header)').each(function() {
+	'use strict';
 	var imdbId = $(this).attr('id').split('-')[1];
 	var omdbUrl = 'http://www.omdbapi.com/?i=' + imdbId;
 	var yearSpan = $(this).find('span.year_column');
 	$.getJSON(omdbUrl, function( data ) {
-		if (data.Response == 'True') {
+		if (data.Response === 'True') {
 			addData(yearSpan, data.imdbRating, data.imdbVotes, data.Released);
 		}
 	});
@@ -49,9 +52,10 @@ $('.filmo-row:not(.header)').each(function() {
 
 // sorts the list of movies/TV Shows
 function sortCategory(sectionDiv, sortBy, direction) {
+	'use strict';
 	var rows = $(sectionDiv).children('div').not('.header').remove();
-	rows.sort(function(a,b){
-		var opA, opB, selector
+	rows.sort(function(a,b) {
+		var opA, opB, selector;
 		switch (sortBy) {
 			case 'Rating':
 				selector = '.rating_column';
@@ -61,30 +65,31 @@ function sortCategory(sectionDiv, sortBy, direction) {
 				break;
 			case 'Year':
 				var aval = $(a).find('.year_column').attr('title');
-				if (aval == 'N/A') {aval = $(a).find('.year_column').text()}
+				if (aval === 'N/A') {aval = $(a).find('.year_column').text();}
 				var bval = $(b).find('.year_column').attr('title');
-				if (bval == 'N/A') {bval = $(b).find('.year_column').text()}
-				return Date.parse(aval) < Date.parse(bval) ? 1 * direction : -1 * direction;
+				if (bval === 'N/A') {bval = $(b).find('.year_column').text();}
+				return Date.parse(aval) < Date.parse(bval) ? (1 * direction) : (-1 * direction);
 				//selector = '.year_column';
-				break;
+				//break;
 			default:
 				break;
 		}
 		opA = Number($(a).find(selector).text().replace(',','').replace('N/A', '0'));
 		opB = Number($(b).find(selector).text().replace(',','').replace('N/A', '0'));
-		return opA < opB ? 1 * direction : (opA > opB ? -1 * direction : 0); 
+		return opA < opB ? (1 * direction) : (opA > opB ? -1 * direction : 0); 
 	});
     // add class odd or even
 	rows.each(function(index) {
 		$(this).removeClass('odd even');
-		if (index % 2 === 0) {$(this).addClass('odd')}
-		else                 {$(this).addClass('even')}
+		if (index % 2 === 0) {$(this).addClass('odd');}
+		else                 {$(this).addClass('even');}
 	});
 	$(sectionDiv).append(rows);
 }
 
 //add click event handlers to the headers (Rating, Votes, Year)
 $('.header div a').click(function () {
+	'use strict';
 	var linkDiv = $(this).parent('div');
 	var direction = 1;
 	if (linkDiv.hasClass('sorted')) {
